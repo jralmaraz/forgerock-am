8a1956134e2c26e76f9eacb7847eb77398a03130
jose.almaraz.ext <jose.almaraz.ext@MacBook-Pro-van-josealmarazext.local>
2023-04-05 13:15:04 +0200

    rename to have terraform-provider prefix - should've done while ago

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/jralmaraz/forgerock-am/accessmanager"
+	"terraform-provider-forgerock-am/accessmanager"
 )
 
 func main() {
